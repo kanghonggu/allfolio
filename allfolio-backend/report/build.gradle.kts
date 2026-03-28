e01646aa355c28c9c751166535dbe951347dabcb
hong9 <dev@qemistry.io>
2026-03-28 23:19:56 +0900

    init

---
+++ allfolio-backend/report/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
