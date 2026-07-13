a8ddeec00669a4041a9eb831e5623f7411f6b1fb
hong9 <dev@qemistry.io>
2026-07-13 14:46:15 +0900

    refactor(unified-asset): SyncAccountUseCase에 AccountSyncRunner seam 도입

---
+++ allfolio-backend/unified-asset/src/main/kotlin/com/allfolio/unifiedasset/application/usecase/AccountSyncRunner.kt
@@ -0,0 +1,12 @@
+package com.allfolio.unifiedasset.application.usecase
+
+import java.util.UUID
+
+/**
+ * 단일 계좌 동기화 실행 seam.
+ * DailyAccountSyncer가 concrete SyncAccountUseCase 대신 이 인터페이스에 의존해
+ * 테스트에서 fake로 대체 가능하게 한다. 유일 구현체는 SyncAccountUseCase.
+ */
+interface AccountSyncRunner {
+    fun execute(accountId: UUID): SyncResult
+}
